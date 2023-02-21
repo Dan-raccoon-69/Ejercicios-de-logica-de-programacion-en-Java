15ff2c131770ad790680e4902acde0eb1495f361
Daniel <gustavodelacruz2010@gmail.com>
2023-02-20 20:56:16 -0600

    Cambios

--- Fibonacci/src/Fibonacci.java
+++ Fibonacci/src/Fibonacci.java
@@ -1,5 +1,25 @@
 public class Fibonacci {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        fibonacci();
+    }
+
+    public static void fibonacci() {
+        int cantidad = 2;
+        long resultado = 0, a = 0, b = 1;
+
+        long array[] = new long[50];
+        array[0] = a;
+        array[1] = b;
+        while (cantidad < 50) {
+            // almacenandolos en un arreglo
+            array[cantidad] = resultado = a + b;
+            a = b;
+            b = resultado;
+            cantidad++;
+        }
+        // mostrando los 50 elementos en el arreglo
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Posicion: " + (i + 1) + " Valor: " + array[i]);
+        }
     }
 }
