94ba9711a9c95aaa725a3ac58c28b7c844fdaab4
Dan-raccoon-69 <gustavodelacruz2010@gmail.com>
2023-05-16 20:14:31 -0600

    Cambios

--- número al reves/src/numAlreves.java
+++ número al reves/src/numAlreves.java
@@ -1,7 +1,11 @@
 public class numAlreves {
     public static void main(String[] args) throws Exception {
         calcular(15);
-        calcular(1558);
+        calcular(15587);
+        calcular(53784);
+        calcular(537);
+        calcular(53);
+        calcular(5378);
     }
 
     public static void calcular(int num) {
@@ -9,11 +13,17 @@ public class numAlreves {
         int newNumeroInt;
         
         numero = String.valueOf(num);
-        for (int i = numero.length() - 1; i >= 0; i--) {
-            newNumeroString += numero.charAt(i);
+        if(numero.length() == 5){
+            for (int i = numero.length() - 1; i >= 0; i--) {
+                newNumeroString += numero.charAt(i);
+            }
+    
+            newNumeroInt = Integer.parseInt(newNumeroString);
+            System.out.println("\nNumero = " + num + "\nNumero Nuevo = " + newNumeroInt);
+        }
+        else{
+            System.out.println("\nEl Número = " + num + " no tiene las 5 cifras");
         }
 
-        newNumeroInt = Integer.parseInt(newNumeroString);
-        System.out.println("\nNumero = " + num + "\nNumero Nuevo = " + newNumeroInt);
     }
 }
